6ee121a62d6a97b239a63f93f65381cc0988a2ec
Yaseen <yassensad5@gmail.com>
2024-11-01 23:37:15 +0300

    first commit

--- src/main/java/com/maids/cc/LibraryManagement/repo/BorrowingRecordRepository.java
+++ src/main/java/com/maids/cc/LibraryManagement/repo/BorrowingRecordRepository.java
@@ -6,6 +6,5 @@ import com.maids.cc.LibraryManagement.model.BorrowingRecord;
 
 public interface BorrowingRecordRepository extends JpaRepository<BorrowingRecord, Long> {
     
-    // Add this method to find a borrowing record by book ID and patron ID
     BorrowingRecord findByBook_IdAndPatron_Id(Long bookId, Long patronId);
 }
